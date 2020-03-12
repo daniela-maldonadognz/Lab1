4be856680c7759568355cede9393b3c3edd36159
Daniela Maldonado Gonzalez <daniela.maldonadognz@uanl.edu.mx>
2020-03-11 20:25:11 -0600

    Se programo video agregar

---
+++ Controllers/VideoController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using System.Data.SqlClient;
+using System.Data;
+
+using MVCPlantilla.Utilerias;
+
+namespace MvcPlantilla.Controllers
+{
+    public class VideoController : Controller
+    {
+        //
+        // GET: /Video/
+
+        public ActionResult Index()
+        {
+            BaseHelper.ejecutarSentencia("INSERT INTO Video VALUES (@idVideo, @titulo, @repro, @url)", CommandType.StoredProcedure, parametros);
+
+
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(int idVideo,string titulo, int repro,string url)
+        {
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@idVideo", idVideo));
+            parametros.Add(new SqlParameter("@titulo", titulo));
+            parametros.Add(new SqlParameter("@repro", repro));
+            parametros.Add(new SqlParameter("@url", url));
+
+            BaseHelper.ejecutarSentencia("SELECT* FROM Video(@idVideo, @titulo, @repro, @url)", CommandType.StoredProcedure, parametros);
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int idVideo, string titulo, int repro, string url)
+        {
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@idVideo", idVideo));
+            parametros.Add(new SqlParameter("@titulo", titulo));
+            parametros.Add(new SqlParameter("@repro", repro));
+            parametros.Add(new SqlParameter("@url", url));
+
+            BaseHelper.ejecutarSentencia("UPDATE Video SET titulo=@titulo, repro=@repro, url=@url WHERE idVideo= @idVideo", CommandType.StoredProcedure, parametros);
+
+            return View();
+            
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int idVideo)
+        {
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@idVideo", idVideo));
+
+            BaseHelper.ejecutarSentencia("DELETE FROM Video WHERE idVideo=@idVideo  ", CommandType.StoredProcedure, parametros);
+            return View();
+        }
+
+  
+    }
+}
---
+++ Views/Video/Index.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" Inherits="System.Web.Mvc.ViewPage<dynamic>" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title>Index</title>
+</head>
+<body>
+    <a href="/Home/Index"> INICIO </a>
+    <br />   
+    <form action="/Video/Index" method= "post">
+        <fieldset>
+            <legend>Datos del video</legend>
+           
+            <label for="idVideo">ID Video:</label>
+            <input type="text" name="idVideo" />
+
+            <label for="titulo">Título:</label>
+            <input type="text" name="titulo" />
+
+            <input type="submit" value="Buscar" />
+            
+            </fieldset>
+    </form>
+</body>
+</html>
